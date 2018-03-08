96a07b2b43ae54902cee3c74e4af851ad5b91a8c
Emil Ramsdal <emil@ervi.no>
2018-03-08 15:14:36 +0100

    Initial Commit

---
+++ MobileTechinalChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileTechinalChallenge
+//
+//  Created by Emil Ramsdal on 08/03/2018.
+//  Copyright © 2018 Emil Ramsdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
